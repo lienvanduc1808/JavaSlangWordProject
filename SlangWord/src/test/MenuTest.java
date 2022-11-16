650bdf29acee71b6554c7024894618ade5d3db6f
lienvanduc1808 <20127467@student.hcmus.edu.vn>
2022-11-16 21:40:08 +0700

    handle File

--- SlangWord/src/test/MenuTest.java
+++ SlangWord/src/test/MenuTest.java
@@ -2,6 +2,11 @@ package test;
 
 public class MenuTest {
 	public static void main(String[] args) {
+		
+		int choose = 0;
+		do {
+			
+		
 		System.out.println("--------------------MENU------------------------");
 		System.out.println("1.  Tìm kiếm theo slang word.");
 		System.out.println("2.  Tìm kiếm theo definition.");
@@ -13,5 +18,8 @@ public class MenuTest {
 		System.out.println("8.  Random 1 slang word .");
 		System.out.println("9.  Đố vui (Random )");
 		System.out.println("10. Đố vui (Definition).");
+		System.out.println("0. Thoát khỏi chương trình.");
+		}
+		while(choose != 0 );
 	}
 }
