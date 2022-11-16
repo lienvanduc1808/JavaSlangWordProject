64e5a7e8259cd05f9bbde95f758961ae084cea40
lienvanduc1808 <20127467@student.hcmus.edu.vn>
2022-11-16 20:08:52 +0700

    Menu

---
+++ SlangWord/src/test/MenuTest.java
@@ -0,0 +1,17 @@
+package test;
+
+public class MenuTest {
+	public static void main(String[] args) {
+		System.out.println("--------------------MENU------------------------");
+		System.out.println("1.  Tìm kiếm theo slang word.");
+		System.out.println("2.  Tìm kiếm theo definition.");
+		System.out.println("3.  Hiển thị history.");
+		System.out.println("4.  Add 1 slang words mới.");
+		System.out.println("5.  Edit 1 slang word.");
+		System.out.println("6.  Delete 1 slang word.");
+		System.out.println("7.  Reset danh sách slang words gốc.");
+		System.out.println("8.  Random 1 slang word .");
+		System.out.println("9.  Đố vui (Random )");
+		System.out.println("10. Đố vui (Definition).");
+	}
+}
